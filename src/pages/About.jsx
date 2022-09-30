dbd0e284f1bc124fedd739016fbd234cc9749860
mehdibouaziz <mehdi.bouaziz@gadz.org>
2022-09-30 14:52:36 +0200

    fix About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -21,10 +21,11 @@ const About = () => {
         Version <span className='text-white'>1.0.0</span>
       </p>
       <p className='text-lg text-gray-400'>
-        Layout By:
+        {'Layout: original by '}
         <a className='text-white' href='https://twitter.com/hassibmoddasser'>
           Hassib Moddasser
         </a>
+        {' and tweaked by Mehdi Bouaziz'}
       </p>
     </>
   )
